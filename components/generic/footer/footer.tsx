a819ce259cc180c377142086072b6339b9cf96f0
maheshhadiya11 <mahesh.hadiya@openmedium.biz>
2026-01-12 16:21:32 +0530

    Update footer.tsx
    accesb

--- components/generic/footer/footer.tsx
+++ components/generic/footer/footer.tsx
@@ -193,7 +193,6 @@ const Footer = async () => {
           </div>
         </div>
       </div>
-      {/* <Script>{accessibilityAdjustments}</Script> */}
        <script> (function(){ var s = document.createElement('script'); var h = document.querySelector('head') || document.body; s.src = 'https://acsbapp.com/apps/app/dist/js/app.js'; s.async = true; s.onload = function(){ acsbJS.init(); }; h.appendChild(s); })(); </script> 
     </footer>
   )
