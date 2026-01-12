c83d99e1d44e1008bf6d0f4d4d853c973146f61c
maheshhadiya11 <mahesh.hadiya@openmedium.biz>
2026-01-12 16:27:56 +0530

    Update footer.tsx
    accessbi

--- components/generic/footer/footer.tsx
+++ components/generic/footer/footer.tsx
@@ -193,7 +193,7 @@ const Footer = async () => {
           </div>
         </div>
       </div>
-       <script> (function(){ var s = document.createElement('script'); var h = document.querySelector('head') || document.body; s.src = 'https://acsbapp.com/apps/app/dist/js/app.js'; s.async = true; s.onload = function(){ acsbJS.init(); }; h.appendChild(s); })(); </script> 
+       <Script> (function(){ var s = document.createElement('script'); var h = document.querySelector('head') || document.body; s.src = 'https://acsbapp.com/apps/app/dist/js/app.js'; s.async = true; s.onload = function(){ acsbJS.init(); }; h.appendChild(s); })(); </Script> 
     </footer>
   )
 }
